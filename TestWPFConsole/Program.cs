941aedfd76d279555d35d31221e0c28f61afd1c1
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-09-05 18:34:47 +0300

    Created test console project

---
+++ TestWPFConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestWPFConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
