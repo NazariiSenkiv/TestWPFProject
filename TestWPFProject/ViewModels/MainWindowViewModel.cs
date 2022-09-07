bf8b1ce5baf2835aa20441e6d4a7a3735175a4ba
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-09-07 15:37:42 +0300

    Added test data for chart

--- TestWPFProject/ViewModels/MainWindowViewModel.cs
+++ TestWPFProject/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 using TestWPFProject.Infrastructure.Commands;
+using TestWPFProject.Models;
 using TestWPFProject.ViewModels.Base;
 
 namespace TestWPFProject.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        #region TestDataPoints
+
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        } 
+
+        #endregion
+
+
         #region Window Title
         private string _Title = "No WPF?";
 
@@ -50,6 +66,18 @@ namespace TestWPFProject.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+            const double step = 0.1;
+            var dataPoints = new List<DataPoint>((int)(360 / step));
+            for (var x = 0d; x < 360; x += step)
+            {
+                const double radsInDeg = Math.PI / 180;
+                var y = Math.Sin(2 * Math.PI * x * radsInDeg);
+
+                dataPoints.Add(new DataPoint { X = x, Y = y });
+            }
+
+            TestDataPoints = dataPoints;
         }
     }
 }
