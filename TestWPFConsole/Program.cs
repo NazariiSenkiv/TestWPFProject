02cc8fbb6fc4178089fa892c38fcaa7ef015830a
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-09-07 13:58:16 +0300

    Added request to remote server to get raw data

--- TestWPFConsole/Program.cs
+++ TestWPFConsole/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace TestWPFConsole
 {
     class Program
     {
+        private const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+
+            var httpResponse = client.GetAsync(dataUrl).Result;
+            var csvStr = httpResponse.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
