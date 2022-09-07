35787e9e5e439323c1429475e8f47d9bd94b8989
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-09-07 11:26:15 +0300

    Added separate close application command class

---
+++ TestWPFProject/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using TestWPFProject.Infrastructure.Commands.Base;
+
+namespace TestWPFProject.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : CommandBase
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
